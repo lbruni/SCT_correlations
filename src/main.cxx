5b3391284702ee77bdeedfc1580e7e14c532d40f
rpeschke <richard.peschke@desy.de>
2015-05-06 16:10:29 +0200

    removed class dummy

--- src/main.cxx
+++ src/main.cxx
@@ -3,6 +3,10 @@
 #include "TFile.h"
 #include "TApplication.h"
 #include "TH2.h"
+
+#include "local_hit_new.h"
+
+
 int main(int argc, char **argv) {
   TApplication theApp("App", &argc, argv);
   TFile* _file0 = new TFile("C:/Users/Argg/OneDrive/alibava/ROOT/missing.root");
@@ -13,22 +17,3 @@ int main(int argc, char **argv) {
   theApp.Run();
 }
 
-
-class correlations{
-
-
-  correlations(const char* name){
-
-    //h2 = new TH2D(name, name, 1000, -10., 10.);
-  }
-
-  void fill(const std::vector<double>& m_id, const std::vector<double>& m_pos, int id){
-    double plane_1, plane_2;
-
-
-    h2->Fill(plane_1, plane_2);
-
-  }
-
-  TH2D* h2 = nullptr;
-};

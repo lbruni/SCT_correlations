cb0657bd19ae4cc0980693ffff50eaacfaf30d3c
rpeschke <richard.peschke@desy.de>
2015-06-25 16:12:45 +0200

    public data access for plane hit

--- sct_plot/inc/internal/plane_hit.hh
+++ sct_plot/inc/internal/plane_hit.hh
@@ -12,6 +12,7 @@ namespace sct_corr{
   };
 
   class track_hits :public plane_hit{
+  public:
     track_hits(Double_t x_, Double_t y_, Double_t chi2_, Double_t phi_, Double_t theta_) :plane_hit(x_, y_),chi2(chi2_),phi(phi_),theta(theta_){}
 
 

df48d848154a067c3b3788e04d84d64d0e669081
rpeschke <richard.peschke@desy.de>
2015-06-29 23:33:13 +0200

    new Class to contain the output of a Run.

---
+++ sct_plot/inc/sct_events/rootEventRunOutput.hh
@@ -0,0 +1,45 @@
+#ifndef rootEventRunOutput_h__
+#define rootEventRunOutput_h__
+#include "sct_events/rootEventBase.hh"
+#include <memory>
+#include "internal/platform.hh"
+
+class TTree;
+namespace sct_corr{
+  class DllExport rootEventRunOutput :public rootEventBase{
+  public:
+    rootEventRunOutput(TTree* tree);
+    rootEventRunOutput(const char* collectionName);
+    rootEventRunOutput();
+    virtual ~rootEventRunOutput(){}
+    virtual std::shared_ptr<plane> createPlane(double ID) override;
+    virtual void Save2Tree(TTree* outputTree) override;
+    void set_TotalNumOfEvents(double numOfEvents);
+    void set_Total_efficiency(double effi);
+    void set_Threshold(double thr);
+    void set_residual(double res);
+    void set_offset(double offset);
+    void set_RunNumber(double RunNum);
+  private:
+
+    std::shared_ptr<double> m_totalNumOfEvents ;
+    std::shared_ptr<double> m_total_efficiency ;
+    std::shared_ptr<double> m_residual ;
+    std::shared_ptr<double> m_offset ;
+    std::shared_ptr<double> m_Threshold ;
+    std::shared_ptr<double> m_RunNumber ;
+    std::shared_ptr<double> m_HV ;
+    std::vector<double> *m_x = nullptr, // these are just shortcuts 
+      *m_y = nullptr,
+      *m_Occupancy=nullptr,
+      *m_NumOfEvents=nullptr,
+      *m_id = nullptr;
+
+  };
+
+
+
+}
+
+
+#endif // rootEventRunOutput_h__
---
+++ sct_plot/src/sct_events/rootEventRunOutput.cc
@@ -0,0 +1,109 @@
+#include "sct_events/rootEventRunOutput.hh"
+#include "TTree.h"
+
+
+
+std::vector<std::string> get_axis_list(){
+  std::vector<std::string> ret;
+  ret.emplace_back(getIDString());
+  ret.emplace_back(axis2String(x_axis_def));
+  ret.emplace_back(axis2String(y_axis_def));
+  ret.emplace_back(axis2String(Occupancy_axis_def));
+  ret.emplace_back(axis2String(NumOfEvents_axis_def));
+  return ret;
+
+}
+namespace sct_corr{
+
+  rootEventRunOutput::rootEventRunOutput(TTree* tree) : rootEventBase(tree),
+    m_x(getData(x_axis_def)),
+    m_y(getData(y_axis_def)),
+    m_Occupancy(getData(Occupancy_axis_def)),
+    m_NumOfEvents(getData(NumOfEvents_axis_def)),
+    m_id(getData(getIDString()))
+  {
+    m_totalNumOfEvents = std::make_shared< double >(0);
+    m_total_efficiency = std::make_shared< double >(0);
+    m_residual = std::make_shared< double >(0);
+    m_offset = std::make_shared< double >(0);
+    m_Threshold = std::make_shared< double >(0);
+    m_RunNumber = std::make_shared< double >(0);
+    m_HV = std::make_shared< double >(0);
+
+
+    tree->SetBranchAddress("totalNumOfEvents", m_totalNumOfEvents.get());
+    tree->SetBranchAddress("total_efficiency", m_total_efficiency.get());
+    tree->SetBranchAddress("residual", m_residual.get());
+    tree->SetBranchAddress("offset", m_offset.get());
+    tree->SetBranchAddress("Threshold", m_Threshold.get());
+    tree->SetBranchAddress("RunNumber", m_RunNumber.get());
+    tree->SetBranchAddress("HV", m_HV.get());
+  }
+
+  rootEventRunOutput::rootEventRunOutput(const char* collectionName) :rootEventBase(collectionName, get_axis_list()),
+    m_x(getData(x_axis_def)),
+    m_y(getData(y_axis_def)),
+    m_Occupancy(getData(Occupancy_axis_def)),
+    m_NumOfEvents(getData(NumOfEvents_axis_def)),
+    m_id(getData(getIDString()))
+  {
+
+
+
+    m_totalNumOfEvents = std::make_shared< double >(0);
+    m_total_efficiency = std::make_shared< double >(0);
+    m_residual = std::make_shared< double >(0);
+    m_offset = std::make_shared< double >(0);
+    m_Threshold = std::make_shared< double >(0);
+    m_RunNumber = std::make_shared< double >(0);
+    m_HV = std::make_shared< double >(0);
+  }
+
+  std::shared_ptr<plane> rootEventRunOutput::createPlane(double ID)
+  {
+    return nullptr;
+  }
+
+  void rootEventRunOutput::Save2Tree(TTree* outputTree)
+  {
+    rootEventBase::Save2Tree(outputTree);
+    outputTree->Branch("totalNumOfEvents", m_totalNumOfEvents.get());
+    outputTree->Branch("total_efficiency", m_total_efficiency.get());
+    outputTree->Branch("residual", m_residual.get());
+    outputTree->Branch("offset", m_offset.get());
+    outputTree->Branch("Threshold", m_Threshold.get());
+    outputTree->Branch("RunNumber", m_RunNumber.get());
+    outputTree->Branch("HV", m_HV.get());
+  }
+
+  void rootEventRunOutput::set_TotalNumOfEvents(double numOfEvents)
+  {
+    *m_totalNumOfEvents = numOfEvents;
+  }
+
+  void rootEventRunOutput::set_Total_efficiency(double effi)
+  {
+    *m_total_efficiency = effi;
+  }
+
+  void rootEventRunOutput::set_Threshold(double thr)
+  {
+    *m_Threshold = thr;
+  }
+
+  void rootEventRunOutput::set_residual(double res)
+  {
+    *m_residual = res;
+  }
+
+  void rootEventRunOutput::set_offset(double offset)
+  {
+    *m_offset = offset;
+  }
+
+  void rootEventRunOutput::set_RunNumber(double RunNum)
+  {
+    *m_RunNumber = RunNum;
+  }
+
+}

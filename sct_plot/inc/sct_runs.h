87650e7262635bc61045a8ca7cbf78ad4b9f9e6e
rpeschke <richard.peschke@desy.de>
2015-07-06 12:55:49 +0200

    added run collection

--- sct_plot/inc/sct_runs.h
+++ sct_plot/inc/sct_runs.h
@@ -9,8 +9,28 @@ public:
   static void DEVICE_1_ASIC_on_Position_7_Jim_350V(s_process_files& p, std::string path__,std::string outputPath=".");
   static void DEVICE_1_ASIC_on_Position_7_Jim_250V(s_process_files& p, std::string path__,std::string outputPath=".");
   static void DEVICE_1_ASIC_on_Position_7_Jim_400V(s_process_files& p, std::string path__,std::string outputPath=".");
-};
 
+
+  static void DEVICE_1_ASIC_on_Position_5_150V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_1_ASIC_on_Position_5_250V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_1_ASIC_on_Position_5_350V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_1_ASIC_on_Position_5_400V(s_process_files& p, std::string path__, std::string outputPath = ".");
+
+
+  static void DEVICE_2_ASIC_on_Position_5_150V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_2_ASIC_on_Position_5_250V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_2_ASIC_on_Position_5_350V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_2_ASIC_on_Position_5_400V(s_process_files& p, std::string path__, std::string outputPath = ".");
+
+
+
+  static void DEVICE_2_ASIC_on_Position_7_150V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_2_ASIC_on_Position_7_250V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_2_ASIC_on_Position_7_350V(s_process_files& p, std::string path__, std::string outputPath = ".");
+  static void DEVICE_2_ASIC_on_Position_7_400V(s_process_files& p, std::string path__, std::string outputPath = ".");
+
+
+};
 
 #ifdef __CINT__
 

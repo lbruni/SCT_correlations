5f355b813463b51f23548de99f5ac9d7afaa4382
lbruni <lbruni@nikhef.nl>
2015-07-07 17:47:48 +0200

    changes

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/Volumes/Macintosh\ HD/Applications/CMake.app/Contents/bin/
+export ROOTSYS=~/Downloads/root/
+export ROOT_SYS=~/Downloads/root/
+export PATH=~/Downloads/root/bin/:$PATH
+export LANDAUGAUS=~/Dropbox/SCt_correlations/landau_gauss/

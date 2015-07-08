6c13086bdc574cc48f2bae420cde6664788191fe
rpeschke <richard.peschke@desy.de>
2015-07-08 10:52:34 +0200

    made it conform with linux

--- sct_plot/src/s_axis.cc
+++ sct_plot/src/s_axis.cc
@@ -1,4 +1,4 @@
-#include "s_axis.h"
+#include "S_Axis.h"
 
 
 S_Axis::S_Axis(const char* collctionName, double planeID, axis_def axis) :m_collectionName(collctionName), m_planeID(planeID), m_axis(axis)

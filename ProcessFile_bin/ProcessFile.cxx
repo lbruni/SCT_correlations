755857320896f3c9bb011185e16bbd674f7a7267
rpeschke <richard.peschke@desy.de>
2015-07-16 11:38:06 +0200

    current status of process file programm

--- ProcessFile_bin/ProcessFile.cxx
+++ ProcessFile_bin/ProcessFile.cxx
@@ -1,6 +1,6 @@
 
-#include "../sct_plot/inc/s_process_files.h"
-#include "../sct_plot/inc/sct_runs.h"
+#include "s_process_files.h"
+
 #include "TError.h"
 
 #include "TApplication.h"
@@ -9,24 +9,53 @@
 #include "TF1.h"
 #include "TProfile.h"
 
+#include "../extern/Scientific_Quantities/include/quantities/units.h"
+#include "TCanvas.h"
+s_process_files* run_residual(){
 
+  const char* inFileName = "D:/data/DEVICE_1_ASIC_on_Position_7_Jim_350V/run000703-fitter.root";
+
+  TFile* _file1 = new TFile("dummy.root", "RECREATE");
+
+
+  s_process_files* p=new s_process_files();
+
+  p->setOutputName("D:/data/DEVICE_1_ASIC_on_Position_7_Jim_350V/dmmy.root");
+  
+  p->SetNumberOfBins(400);
+  p->AddCut(S_YCut(-4, 2));
+  p->setActiveArea(280, 360);
+  p->SetRotation(0.0171112);
+  p->SetPosition(3.16097e+002, 0);
+  p->push_files(inFileName, 50, 703, 350);
+  
+  p->process();
+  p->DrawResidual(-2, 2);
+  new TCanvas();
+  p->Draw_Efficinecy_map();
+  return p;
+}
 int main(int argc, char **argv) {
 
   TApplication theApp("App", &argc, argv);
-  s_process_files p;
+//  s_process_files p;
 
  
   gErrorIgnoreLevel = kError;  // ignoring root printouts (replace of histograms) 
-  TFile * __file = new TFile("D:/data/DEVICE_1_ASIC_on_Position_5_150V/run000893-fitter.root");
-  TFile * __file1 = new TFile("dummy.root","recreate");
+//   TFile * __file = new TFile("D:/data/DEVICE_1_ASIC_on_Position_5_150V/run000893-fitter.root");
+//   TFile * __file1 = new TFile("dummy.root","recreate");
+// 
+// 
+//   p.push_files(__file, 50, 893);
+//   p.setOutputName("test.root");
+//   p.process();
+// 
+//   
+//   p.DrawResidualVsMissingCordinate(-255, -240);
 
-
-  p.push_files(__file, 50, 893);
-  p.setOutputName("test.root");
-  p.process();
+  auto p = run_residual();
 
   
-  p.DrawResidualVsMissingCordinate(-255, -240);
 //   auto h2 = p.getResidualVsMissingCordinate();
 // 
 //   SCT_helpers::CutTH2(h2, S_Cut_BinContent(20));

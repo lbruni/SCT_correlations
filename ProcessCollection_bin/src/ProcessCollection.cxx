b3085c342c52307bcd56b0bfd80594f49cad964a
rpeschke <richard.peschke@desy.de>
2015-07-24 09:30:49 +0200

    moved xml read in class into its own header file

--- ProcessCollection_bin/src/ProcessCollection.cxx
+++ ProcessCollection_bin/src/ProcessCollection.cxx
@@ -2,127 +2,15 @@
 #include "s_process_files.h"
 #include "TError.h"
 
-#include "rapidxml.hpp"
-#include "rapidxml_utils.hpp"
 #include <iostream>
 #include <sstream>
-#include "rapidxml.hpp"
-#include "xml_helpers/xml_util.hh"
 #include "tclap/CmdLine.h"
+#include "xml_helpers/xml_fileList.hh"
 
 using namespace xml_util;
 
 using namespace rapidxml;
 
-namespace xmlImputFiles{
-
-
-
-  using xml_n = xml_node < char > ;
-  template<typename T>
-  class MinMaxRange{
-  public:
-    MinMaxRange(T min_, T max_) :m_min(min_), m_max(max_){}
-    T getMin() const{
-      return m_min;
-    }
-    T getMax() const{
-      return m_max;
-    }
-  private:
-    T m_min, m_max;
-  };
-  using intRange = MinMaxRange < int > ;
-  class xml_globalConfig{
-  public:
-    xml_globalConfig(xml_n* xIn) :m_node(xIn){
-      // std::cout << xIn->name << std::endl;
-    }
-    xml_globalConfig(){}
-    std::string CollectionName() const {
-      return getAtribute(m_node->first_node("CollectionName"), "value", "notSet");
-    }
-    int NumberOfBins() const{
-      return getAtribute(m_node->first_node("NumberOfBins"), "value", -1);
-    }
-    S_Cut cut() const{
-      auto min_ = getAtribute(m_node->first_node("YCut"), "min", -1);
-      auto max_ = getAtribute(m_node->first_node("YCut"), "max", -1);
-      return S_YCut(min_, max_);
-    }
-    int Device() const{
-      return getAtribute(m_node->first_node("Device"), "value", -1);
-    }
-
-    intRange AvtiveStrips() const{
-      auto min_ = getAtribute(m_node->first_node("AvtiveStrips"), "min", -1);
-      auto max_ = getAtribute(m_node->first_node("AvtiveStrips"), "max", -1);
-      return intRange(min_, max_);
-    }
-    double Rotation()const {
-      return getAtribute(m_node->first_node("Rotation"), "value", 0.0);
-    }
-    std::string Position_name()const{
-      return getAtribute(m_node->first_node("Position"), "name", "notSet");
-    }
-    double Position_value()const {
-      return getAtribute(m_node->first_node("Position"), "value", -1.0);
-    }
-  private:
-    xml_n* m_node = nullptr;
-  };
-  class xml_file{
-  public:
-    xml_file(xml_n* xIn) :
-      m_node(xIn){
-
-    }
-    static const char* NodeName() {
-      return "file";
-    }
-    xml_file(){}
-    std::string name() const {
-      return getAtribute(m_node->first_node("name"), "value", "notSet");
-    }
-    double threshold() const {
-      return getAtribute(m_node->first_node("threshold"), "value", -1.0);
-    }
-    double HV() const{
-      return getAtribute(m_node->first_node("HV"), "value", -1.0);
-    }
-    int runNumber() const {
-      return getAtribute(m_node->first_node("runNumber"), "value", -1);
-    }
-  private:
-    xml_n* m_node = nullptr;
-  };
-  class XML_imput_file{
-  public:
-    XML_imput_file(const char* name) :m_file(name) {
-      m_doc.parse<0>(m_file.data());    // 0 means default parse flags
-      //      auto p = m_doc.next_sibling("");
-      m_globalConfig = xml_globalConfig(m_doc.first_node("RunCollection")->first_node("globalConfig"));
-
-      m_files = getVectorOfT<xml_file>(m_doc.first_node("RunCollection")->first_node("fileList"));
-
-    }
-
-    const xml_globalConfig& globalConfig() const {
-      return m_globalConfig;
-    }
-    const std::vector<xml_file>& fileList() const{
-      return m_files;
-
-    }
-
-
-  private:
-    xml_document<> m_doc;
-    rapidxml::file<> m_file;
-    xml_globalConfig m_globalConfig;
-    std::vector<xml_file> m_files;
-  };
-}
 
 int ADDRun(s_process_files& p, std::string xmlInputFileName, std::string path__, std::string outputPath = "."){
   path__ += "/";

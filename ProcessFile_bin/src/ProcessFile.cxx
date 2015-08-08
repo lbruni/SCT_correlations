bad416dfb162e361e075730669ba25bc77ba05c2
rpeschke <richard.peschke@desy.de>
2015-08-08 13:11:55 +0200

    reverted change. now the function runs like before. and uses the commandline input

--- ProcessFile_bin/src/ProcessFile.cxx
+++ ProcessFile_bin/src/ProcessFile.cxx
@@ -272,7 +272,7 @@ int main(int argc, char **argv) {
   para.argc = argc;
   para.argv = argv;
   std::cout << "press q to quit the program" << std::endl;
-  std::thread thr(asyncMainTest, &para);
+  std::thread thr(asyncMain, &para);
   thr.detach();
   std::string i;
   while (i!="q") {

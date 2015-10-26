4bb2a1562a60018e3c3d46798237a5e51c45bc69
rpeschke <richard.peschke@desy.de>
2015-10-26 11:39:44 +0100

    updated play script

--- run_GBL_bin/src/run_GBL.cxx
+++ run_GBL_bin/src/run_GBL.cxx
@@ -69,21 +69,145 @@ int asyncMain(void *arg) {
   std::string path_ = "D:/GBL/DEVICE_1_ASIC_on_Position_7_Jim_350V/";
   std::string name_ = "run000688_";
   std::string name_suffix = "fitter";
-  std::string extension_="root";
-  std::string fullName = path_ + name_ + name_suffix+"." + extension_;
-  
- //TFile * file_ = new TFile(fullName.c_str());
-//   rapidxml::file<> m_file("D:/GBL/DEVICE_1_ASIC_on_Position_7_Jim_350V/alignedGear-check-iter2-run000703_with_plane20.xml");
-//   rapidxml::xml_document<> m_doc;
-//   m_doc.parse<0>(m_file.data());
-// 
-//   auto gear = sct_corr::Xgear(m_doc.first_node("gear"));
-//  auto gear = sct_corr::load_gear("D:/GBL/DEVICE_1_ASIC_on_Position_7_Jim_350V/alignedGear-check-iter2-run000703_with_plane20.xml");
+  std::string extension_ = "root";
+  std::string fullName = path_ + name_ + name_suffix + "." + extension_;
 
- // r_plot_collection pl(file_);
-//  pl.setOutputFile(out_file);
+  //TFile * file_ = new TFile(fullName.c_str());
+  //   rapidxml::file<> m_file("D:/GBL/DEVICE_1_ASIC_on_Position_7_Jim_350V/alignedGear-check-iter2-run000703_with_plane20.xml");
+  //   rapidxml::xml_document<> m_doc;
+  //   m_doc.parse<0>(m_file.data());
+  // 
+  //   auto gear = sct_corr::Xgear(m_doc.first_node("gear"));
+  //  auto = sct_corr::load_gear("D:/GBL/DEVICE_1_ASIC_on_Position_7_Jim_350V/alignedGear-check-iter2-run000703_with_plane20.xml");
+
+  // r_plot_collection pl(file_);
+  //  pl.setOutputFile(out_file);
+
+
+#if 0
+  TFile * file_ = new TFile("C:/Users/Argg/Downloads/hitmaker(1).root");
+
+  TFile * file_1 = new TFile("Dummy.root", "recreate");
+  auto pl1 = sct_corr::create_plot_collection();
+
+
+  pl1->addFile(file_);
+  pl1->setOutputFile(file_1);
+  sct_corr::plane_def sz_dut(sct_type::collectionName_t("hit"), sct_type::ID_t(8), nullptr);
+  sz_dut.set_plot_collection(pl1);
+  sct_corr::plane_def sz_tel(sct_type::collectionName_t("hit"), sct_type::ID_t(5), nullptr);
+  sz_tel.set_plot_collection(pl1);
+
+
+  // auto cuted = sct_corr::processor::cut_x_y(sz_tel, S_XCut(200) + S_YCut(200));
+  // auto cuted_dut = sct_corr::processor::cut_x_y(sz_dut, S_XCut(1060, 1180));
+  auto corr = sct_corr::processor::correlation(sz_dut.getX_def(), sz_tel.getY_def(), s_plot_prob().SaveToDisk());
+  auto corryx = sct_corr::processor::correlation(sz_dut.getY_def(), sz_tel.getX_def(), s_plot_prob().SaveToDisk());
+  auto corrxx = sct_corr::processor::correlation(sz_dut.getX_def(), sz_tel.getX_def(), s_plot_prob().SaveToDisk());
+  auto corryy = sct_corr::processor::correlation(sz_dut.getY_def(), sz_tel.getY_def(), s_plot_prob().SaveToDisk());
+
+  auto sz_dut_normalized = sct_corr::processor::coordinate_transform(sz_dut, -1, 0, -1, 0, s_plot_prob().doNotSaveToDisk());
+  auto sz_tel_normalized = sct_corr::processor::coordinate_transform(sz_tel, 1, 0, 1, 0, s_plot_prob().doNotSaveToDisk());
+  auto resyy = sct_corr::processor::residual(sz_dut_normalized.getY_def(), sz_tel_normalized.getY_def());
+
+  auto resxy = sct_corr::processor::residual(sz_dut_normalized.getX_def(), sz_tel_normalized.getY_def());
+
+  auto resyx = sct_corr::processor::residual(sz_dut_normalized.getY_def(), sz_tel_normalized.getX_def());
+
+  auto resxx = sct_corr::processor::residual(sz_dut_normalized.getX_def(), sz_tel_normalized.getX_def());
+
+  pl1->loop(100000);
+  new TCanvas();
+  TH2D h1("h1", "DUT x Vs Tel y hist", 100, 0, 0, 100, 0, 0);
+  SCT_helpers::Draw<TH2>(corr, S_DrawOption().opt_colz().output_object(&h1));
+  //  auto p = SCT_helpers::LinearFit_Of_Profile(&h1, sct_type::procent_t(70));
+  // h1.Draw("colz");
+  // p.Draw("same");
+  // p.Print();
+  new TCanvas();
+
+  TH2D h2("h2", "DUT x Vs Tel y hist", 1000, 0, 0, 1000, 0, 0);
+
+  SCT_helpers::Draw<TH2>(corryx, S_DrawOption().opt_colz().output_object(&h2))->SetTitle("DUT y VS Tel x");
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(corrxx, S_DrawOption().opt_colz())->SetTitle("DUT x VS Tel x");
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(corryy, S_DrawOption().opt_colz())->SetTitle("DUT y VS Tel Y");
+  new TCanvas();
+
+  TH2D h3("h3", "DUT x Vs Tel y hist", 100, 0, 0, 100, 0, 0);
+
+  SCT_helpers::Draw<TH2>(resyy, S_DrawOption().opt_colz().draw_x_VS_y())->SetTitle("DUT y - TEL y");
+
+
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(resyx, S_DrawOption().opt_colz().draw_x_VS_y())->SetTitle("DUT y - TEL x");
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(resxy, S_DrawOption().opt_colz().draw_x_VS_y())->SetTitle("DUT x - TEL y");
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(resxx, S_DrawOption().opt_colz().draw_x_VS_y())->SetTitle("DUT x - TEL x");
+
+
+  gBrowser = new TBrowser();
+  theApp.Run();
+
+
+
+#endif
+#if 1
+
+
+  TFile * file_ = new TFile("C:/Users/Argg/Documents/Neuer Ordner/gitHub/eudaq/bin/test000580.root");
+
+  TFile * file_1 = new TFile("Dummy.root", "recreate");
+  auto pl1 = sct_corr::create_plot_collection();
+
+
+  pl1->addFile(file_);
+  pl1->setOutputFile(file_1);
+  sct_corr::plane_def sz_dut(sct_type::collectionName_t("szData"), sct_type::ID_t(8), nullptr);
+  sz_dut.set_plot_collection(pl1);
+  sct_corr::plane_def sz_tel(sct_type::collectionName_t("szData"), sct_type::ID_t(7), nullptr);
+  sz_tel.set_plot_collection(pl1);
+
+
+  auto cuted = sct_corr::processor::cut_x_y(sz_tel, S_XCut(0));
+  auto cuted_dut = sct_corr::processor::cut_x_y(sz_dut, S_XCut(1060, 1180));
+  auto corr = sct_corr::processor::correlation(cuted_dut.getX_def(), cuted.getY_def(), s_plot_prob().SaveToDisk());
+  auto corryx = sct_corr::processor::correlation(cuted_dut.getY_def(), cuted.getX_def(), s_plot_prob().SaveToDisk());
+  auto corryy = sct_corr::processor::correlation(cuted_dut.getY_def(), cuted.getY_def(), s_plot_prob().SaveToDisk());
+
+  auto sz_dut_normalized = sct_corr::processor::coordinate_transform(sz_dut, 1, 0, 1, 0, s_plot_prob().doNotSaveToDisk());
+  auto sz_tel_normalized = sct_corr::processor::coordinate_transform(cuted, 3.19042, -978.631, 1, 0, s_plot_prob().doNotSaveToDisk());
+  auto res = sct_corr::processor::residual(sz_dut_normalized.getX_def(), sz_tel_normalized.getX_def());
+
+  auto corrxx = sct_corr::processor::correlation(sz_dut_normalized.getX_def(), sz_tel_normalized.getX_def(), s_plot_prob().SaveToDisk());
+  pl1->loop(100000);
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(corr, S_DrawOption().opt_colz());
+
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(corryx, S_DrawOption().opt_colz())->SetTitle("DUT y VS Tel x");
+  new TCanvas();
+  TH2D h1("h1", "DUT x Vs Tel y hist", 100, 0, 0, 100, 0, 0);
+  SCT_helpers::Draw<TH2>(corrxx, S_DrawOption().opt_colz().output_object(&h1))->SetTitle("DUT x VS Tel x");
+  auto p = SCT_helpers::LinearFit_Of_Profile(&h1, sct_type::procent_t(40));
+  h1.Draw("colz");
+  p.Draw("same");
+  p.Print();
+
+
+  new TCanvas();
+  SCT_helpers::Draw<TH2>(corryy, S_DrawOption().opt_colz())->SetTitle("DUT y VS Tel Y");
+  new TCanvas();
+  SCT_helpers::Draw(res, S_DrawOption().opt_colz().draw_x_VS_y());
+
+  gBrowser = new TBrowser();
+  theApp.Run();
+#endif // 0
+
   sct_files::fitter_file file___(
-    fullName.c_str(), 
+    fullName.c_str(),
     "D:/GBL/DEVICE_1_ASIC_on_Position_7_Jim_350V/alignedGear-check-iter2-run000703_with_plane20.xml"
     );
 
@@ -91,7 +215,7 @@ int asyncMain(void *arg) {
   TFile * out_file = new TFile("output.root", "recreate");
   auto pl = file___.get_collection();
   pl->setOutputFile(out_file);
-  
+
 
 
   auto gbl_collection = file___.get_correlations_channel(
@@ -101,34 +225,34 @@ int asyncMain(void *arg) {
     move_t(-6.36702e-001),
     s_plot_prob("GBL").SaveToDisk()
     );
- 
 
 
-    auto trueHit_with_tdc_cut = sct_corr::processor::if_a_get_b(
-      file___.DUT_TTC_data(),
-      gbl_collection.getTotalTrueHits(), 
-      S_XCut(0, 100),
-      s_plot_prob("withTDC_CUT")
-      );
+
+  auto trueHit_with_tdc_cut = sct_corr::processor::if_a_get_b(
+    file___.DUT_TTC_data(),
+    gbl_collection.getTotalTrueHits(),
+    S_XCut(0, 100),
+    s_plot_prob("withTDC_CUT")
+    );
 
 
-    auto  second_nearest = sct_processor::find_nearest_strip(
-      gbl_collection.getTotalTrueHits(),
+  auto  second_nearest = sct_processor::find_nearest_strip(
+    gbl_collection.getTotalTrueHits(),
     file___.DUT_zs_data(),
-    x_axis_def, 
-    3, 
+    x_axis_def,
+    3,
     "first"
     );
-  
+
   auto mod = sct_processor::modulo(
-    second_nearest.getHitOnPlaneA(), 
-    modulo_t(3), 
+    second_nearest.getHitOnPlaneA(),
+    modulo_t(3),
     x_axis_def
     );
 
 
   auto rmap = sct_corr::processor::correlation(
-    mod.getModulo().getX_def(), 
+    mod.getModulo().getX_def(),
     second_nearest.getResidual().getX_def()
     );
 
@@ -149,19 +273,19 @@ int asyncMain(void *arg) {
     s_plot_prob("effi")
     );
   sct_corr::inStripClusterSize cl_instrip(
-    gbl_collection.getTrueHitsWithDUT(), 
+    gbl_collection.getTrueHitsWithDUT(),
     file___.DUT_zs_data(), 10,
-    x_axis_def, 
+    x_axis_def,
     modulo_t(3),
     s_plot_prob("cluster_size_instrip").SaveToDisk()
     );
 
   sct_corr::residual_efficienct res_eff(
-    gbl_collection.getTotalTrueHits(), 
-    second_nearest.getHitOnPlaneB(), 
-    S_XCut(280,360),
+    gbl_collection.getTotalTrueHits(),
+    second_nearest.getHitOnPlaneB(),
+    S_XCut(280, 360),
     stripNr_t(400),
-    x_axis_def, 
+    x_axis_def,
     s_plot_prob("Res_efficiency")
     );
 
@@ -176,16 +300,16 @@ int asyncMain(void *arg) {
 
   res_eff.Draw();
   SCT_helpers::saveTH1_as_txt(*res_eff.getEfficiency_map(), (path_ + name_ + "residual_efficiency" + "." + "txt").c_str());
-  
+
   gCanvas.push_back(new TCanvas());
   instrip.Draw(S_DrawOption());
   std::cout << "Efficiency " << instrip.get_total_efficiency() << std::endl;
   SCT_helpers::saveTH1_as_txt(*instrip.getEfficiency_map(), (path_ + name_ + "instripEffi" + "." + "txt").c_str());
 
   gCanvas.push_back(new TCanvas());
-  
+
   eff.Draw();
- 
+
   gCanvas.push_back(new TCanvas());
 
   cl_instrip.Draw();
